e2cdd7afe4b0b9dcf2b5e79add04d69dcdf01e34
大谷東彦 <haruhiko@weseek.co.jp>
2020-08-09 00:57:34 +0900

    - step5

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+	float fallSpeed;
+	float rotSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		this.fallSpeed = 0.01f + 0.1f * Random.value;
+		this.rotSpeed = 6f + 3f * Random.value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.Translate( 0, -fallSpeed, 0, Space.World);
+		transform.Rotate(0, 0, rotSpeed );
+		if (transform.position.y < -5.5f) {
+			Destroy (gameObject);
+		}
+    }
+}
